c8ff07fb8b24a3a7045f0ac1e2dc3655e8444aad
Cameron Messinides <cameron.messinides@gmail.com>
2025-10-28 22:15:41 -0400

    Split KWML inline scanner into separate file

--- internal/kwml/inline.go
+++ internal/kwml/inline.go
@@ -4,79 +4,12 @@ import (
 	"slices"
 	"strings"
 	"unicode"
-	"unicode/utf8"
 )
 
 type delimiter struct {
 	active  bool
 	literal string
 	node    int
-}
-
-type scanner struct {
-	raw                  []byte
-	index                int
-	prev, curr           rune
-	prevWidth, currWidth int
-}
-
-func (s *scanner) advance() (char rune, width int) {
-	if s.isAtEnd() {
-		return
-	}
-
-	s.prev = s.curr
-	s.prevWidth = s.currWidth
-	s.curr, s.currWidth = utf8.DecodeRune(s.raw[s.index:])
-	s.index += s.currWidth
-	return s.curr, s.currWidth
-}
-
-func (s *scanner) peek() (char rune, width int) {
-	if s.isAtEnd() {
-		return
-	}
-
-	return utf8.DecodeRune(s.raw[s.index:])
-}
-
-func (s *scanner) match(char rune) bool {
-	if s.isAtEnd() {
-		return false
-	}
-
-	next, _ := s.peek()
-	return next == char
-}
-
-func (s *scanner) cursor() []byte {
-	return s.raw[s.index:s.index]
-}
-
-func (s *scanner) current() (char rune, width int) {
-	return s.curr, s.currWidth
-}
-
-func (s *scanner) currentBytes() []byte {
-	i := s.index
-	h := i - s.currWidth
-	return s.raw[h:i]
-}
-
-func (s *scanner) previous() (char rune, width int) {
-	return s.prev, s.prevWidth
-}
-
-func (s *scanner) isAtEnd() bool {
-	return s.index >= len(s.raw)
-}
-
-func (s *scanner) isEmpty() bool {
-	return len(s.raw) == 0
-}
-
-func newScanner(raw []byte) *scanner {
-	return &scanner{raw: raw}
 }
 
 type inlineParser struct {
